f0794be27ec41ed7fb0edfaf74b6c5bbc2627f4f
PrajeevanS <prajee.bajee@gmail.com>
2026-07-07 11:36:06 +0530

    feat: add OkumuraHata Propagation

--- models/propagation.py
+++ models/propagation.py
@@ -45,14 +45,58 @@ class FreeSpacePropagation(PropagationModel):
 
 
 class OkumuraHataPropagation(PropagationModel):
-    """
-    Placeholder.
 
-    Will be implemented later.
-    """
+    def __init__(
+        self,
+        base_height=30,
+        mobile_height=1.5,
+        environment="urban"
+    ):
+        self.base_height = base_height
+        self.mobile_height = mobile_height
+        self.environment = environment.lower()
 
-    def path_loss(self, distance_km: float, frequency_mhz: float) -> float:
-        raise NotImplementedError("Okumura-Hata model not implemented yet.")
+    def path_loss(self, distance_km, frequency_mhz):
+
+        if distance_km <= 0:
+            distance_km = 0.001
+
+        hb = self.base_height
+        hm = self.mobile_height
+        f = frequency_mhz
+        d = distance_km
+
+        # Mobile antenna correction factor
+        a_hm = (
+            (1.1 * math.log10(f) - 0.7) * hm
+            - (1.56 * math.log10(f) - 0.8)
+        )
+
+        # Urban Path Loss
+        path_loss = (
+            69.55
+            + 26.16 * math.log10(f)
+            - 13.82 * math.log10(hb)
+            - a_hm
+            + (44.9 - 6.55 * math.log10(hb))
+            * math.log10(d)
+        )
+
+        # Environment correction
+        if self.environment == "suburban":
+            path_loss -= (
+                2 * (math.log10(f / 28)) ** 2
+                + 5.4
+            )
+
+        elif self.environment == "rural":
+            path_loss -= (
+                4.78 * (math.log10(f)) ** 2
+                - 18.33 * math.log10(f)
+                + 40.94
+            )
+
+        return path_loss
 
 
 class COST231Propagation(PropagationModel):
